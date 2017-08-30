8e9a614cbcdd9139cc429af304a1b3d6ea1a43a3
Gea-Suan Lin <gslin@gslin.org>
2017-08-31 00:15:08 +0800

    Read config file and create Net::Twitter object.

--- twitterlist2feed.pl
+++ twitterlist2feed.pl
@@ -4,8 +4,20 @@ use 5.010;
 use strict;
 use warnings;
 
+use Config::IniFiles;
+use Net::Twitter;
+
 do {
     die if !defined $ARGV[1];
+
+    my $cnf = Config::IniFiles->new(-file => $ENV{HOME} . '/.config/twitterlist2feed/config.ini');
+    my $nt = Net::Twitter->new(
+        traits => [qw/API::RESTv1_1/],
+        consumer_key => $cnf->val('default', 'consumer_key'),
+        consumer_secret => $cnf->val('default', 'consumer_secret'),
+        access_token => $cnf->val('default', 'access_token'),
+        access_token_secret => $cnf->val('default', 'access_token_secret'),
+    );
 
     my $owner_screen_name = $ARGV[0];
     my $slug = $ARGV[1];
